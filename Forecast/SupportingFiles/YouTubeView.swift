edc60d2a73104b1d1baa5dc0bb9aee4f16ef3a9b
Benson <benson.yeh.phone@gmail.com>
2024-01-02 21:34:05 +0800

    new

--- Forecast/SupportingFiles/YouTubeView.swift
+++ Forecast/SupportingFiles/YouTubeView.swift
@@ -11,45 +11,52 @@ import SwiftUI
 struct YouTubeView: View {
     @ObservedObject var viewModel = YouTubeViewModel()
     @State private var selectedVideoId: String?
-    let apiKey = "AIzaSyAJdN-ubIOs62NwJ9Kq5aJI9LFR1LJEJMw"  // 你的 YouTube Data API Key
-    
+    let apiKey = "AIzaSyAJdN-ubIOs62NwJ9Kq5aJI9LFR1LJEJMw"
+    let i=1
     var body: some View {
-        ScrollView {
-            VStack {
-                ForEach(viewModel.videos.indices, id: \.self) { index in
-                    let video = viewModel.videos[index]
-                    
+        if #available(iOS 16.0, *) {
+            NavigationStack{
+                ScrollView {
                     VStack {
-                        NavigationLink(
-                            destination: SafariView(url: URL(string: "https://www.youtube.com/watch?v=\(video.id)")!),
-                            isActive: Binding(
-                                get: { selectedVideoId == video.id },
-                                set: { _ in selectedVideoId = nil }
-                            )
-                        ) {
-                            Button(action: {
-                                print("Opening URL: \(video.id)")
-                                selectedVideoId = video.id
-                            }) {
-                                VStack {
-                                    RemoteImage(url: video.snippet.thumbnails.medium.url)
-                                        .aspectRatio(contentMode: .fit)
-                                        .frame(height: 200)
-                                    Text("Weather Report Number \(index + 1)") // 使用 index + 1 显示正确的季节编号
-                                        .font(.caption)
-                                        .foregroundColor(.black)
+                        ForEach(viewModel.videos.prefix(6), id: \.id) { video in
+                            VStack {
+                                NavigationLink(
+                                    destination: SafariView(url: URL(string: "https://www.youtube.com/watch?v=\(video.id)")!),
+                                    isActive: Binding(
+                                        get: { selectedVideoId == video.id },
+                                        set: { _ in selectedVideoId = nil }
+                                    )
+                                ) {
+                                    Button(action: {
+                                        print("Opening URL: \(video.id)")
+                                        selectedVideoId = video.id
+                                    }) {
+                                        VStack(){
+                                            RemoteImage(url: video.snippet.thumbnails.medium.url)
+                                                                                       .aspectRatio(contentMode: .fit)
+                                                                                       .frame(height: 200)
+                                                                                   Text("Weather forecast")
+                                                                                       .font(.caption)
+                                                                                       .foregroundColor(.black)
+
+                                        }
+                                       
+                                        //i+=1
+                                    }
                                 }
                             }
                         }
                     }
+                    .onAppear {
+                        viewModel.fetchYouTubeVideos(videoIds: ["vJItfjchxKo", "JEImttY3mIY", "9V-41hlitsg", "2SnsabHViQI","J6EtjwPlL9w","0Z0piYKKLl8"], apiKey: apiKey)
+                    }
                 }
             }
-            .onAppear {
-                viewModel.fetchYouTubeVideos(videoIds: ["vJItfjchxKo", "JEImttY3mIY", "9V-41hlitsg", "2SnsabHViQI","J6EtjwPlL9w","0Z0piYKKLl8"], apiKey: apiKey)
-            }
+        } else {
+            // Fallback on earlier versions
+        }
         }
     }
-}
 
 struct RemoteImage: View {
     let url: String
